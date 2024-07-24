0d2bd279954b5779bd399c52c535bc23b390deac
RIngerslev <101323949+RIngerslev@users.noreply.github.com>
2024-07-24 20:32:21 +0200

    new

--- app/page.tsx
+++ app/page.tsx
@@ -2,112 +2,165 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div>
+      <div id="root">
+              <div id="nav">
+                  <a href="#home"><h3 className="logo">IngerslevSoftware</h3></a>
+                  <div id="buttonMenu" className="btn not-active"><span></span><span></span><span></span></div>
+                  <ul className="navigation">
+                      <li>
+                          <a href="#home">Home</a>
+                      </li>
+                      <li>
+                          <a href="#about">About</a>
+                      </li>
+                      <li>
+                          <a href="#projects">Projects</a>
+                      </li>
+                      <li>
+                          <a href="#contact">Contact</a>
+                      </li>
+                  </ul>
+              </div>
         </div>
-      </div>
+        
+        <section id="home" className="profileSection">
+            <div className="container">
+                <div className="content">
+                    <div className="profile-main">
+                        <div className="profile-text">
+                            <h1>Frontend/Backend <br/> Developer <Image src="/images/coding.svg" className="laptop" alt="laptop icon"/></h1>
+                            <p>Hello, I'm Rasmus Ingerslev. Welcome to my portfolio. I'm a dedicated Developer based in Aarhus, Denmark.</p>
+                        </div>
+                        <Image src="images/punktbilledeRamme.png" alt="" className="profile" alt="ramme"/>
+                    </div>
+                    <div className="skills">
+                        <p>Primary Languages</p>
+                        <div className="logos">
+                            <ul>
+                                <li>
+                                    <Image src="/images/java-original-wordmark.svg" title="Java" alt="java logo"/>
+                                </li>
+                                <li>
+                                    <Image src="/images/csharp.svg" title="C#" alt="c# logo"/>
+                                </li>
+                                <li>
+                                    <Image src="/images/swift.svg" title="SwiftUI" alt="swift logo"/>
+                                </li>
+                                <li>
+                                    <Image src="/images/javascript-js.svg" title="JavaScript" alt="JavaScript logo"/>
+                                </li>
+                                <li>
+                                    <Image src="/images/html.svg" title="HTML5" alt="html logo"/>
+                                </li>
+                                <li>
+                                    <Image src="/images/css-3.svg" title="CSS3" alt="css logo"/>
+                                </li>
+                            </ul>
+                        </div> 
+                    </div>
+                </div>
+            </div>
+        </section>
 
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+        <section className="about" id="about">
+            <div className="container">
+                <h4>About me</h4>
+                <div className="about-content">
+                    <div className="text-side">
+                        <h4>Frontend/Backend Developer</h4>
+                        <p>As a Developer, I am getting better in different skills. Most of my Education has focused on Java, JavaScript and Swift. I am collaborative by nature, I work seamlessly in teams to deliver exceptional applications. My commitment to staying updated with the latest technologies guarantees quality code and optimal solutions.</p>
+                    </div>
+                    <div className="img-side">
+                        <div className="placement">
+                            <p>Backend & Mobile</p>
+                            <Image src="/images/java-original-wordmark.svg" title="Java" className="skill-icon" alt="skill-icon"/>
+                            <Image src="/images/csharp.svg" title="C#" className="skill-icon" alt="skill-icon"/>
+                            <Image src="/images/nodejs.svg" title="NodeJS" className="skill-icon" alt="skill-icon"/>
+                            <Image src="/images/swift.svg" title="SwiftUI" className="skill-icon" alt="skill-icon"/>
+                            <Image src="/images/reactNative.png" title="ReactNative" className="skill-icon" alt="skill-icon"/>
+                            <p>Frontend</p>
+                            <Image src="/images/javascript-js.svg" title="Javascript" className="skill-icon" alt="skill-icon"/>
+                            <Image src="/images/html.svg" title="HTML5" className="skill-icon" alt="skill-icon"/>
+                            <Image src="/images/css-3.svg" title="CSS3" className="skill-icon" alt="skill-icon"/>
+                            <Image src="/images/pug.svg" title="Pug" className="skill-icon" alt="skill-icon"/>
+                            <p>Database</p>
+                            <Image src="/images/database-sql.svg" title="MS SQL" className="skill-icon" alt="skill-icon"/>
+                            <Image src="/images/file-type-firebase.svg" title="Firebase" className="skill-icon" alt="skill-icon"/>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+        <section id="projects" className="project">
+            <div className="container">
+                <h4>Portfolio</h4>
+                <div className="project-content">
+                  <div className="projects-grid">
+                      <div className="pro pro__1 undefined">
+                          <div className="pro__img">
+                              <a target="_blank" href="https://play.google.com/store/apps/details?id=com.UrDiscFriend" rel="noreferrer">
+                                  <Image src="/images/UrDiscFriend.png" alt="website" className="transform: translateY(0%); transition: transform 10s ease-in-out 0s;"/>
+                              </a>
+                          </div>
+                          <div className="pro__text">
+                              <h3>Ur Disc Friend</h3>
+                              <p>The essential companion for seasoned players. Whether it's a casual enthusiast or pro. Helps players elevate their game with an intuitive, user-friendly interface.</p>
+                              <div className="stack">
+                                  <p>React Native</p>
+                              </div>
+                              <div className="links">
+                                  <a target="_blank" href="https://play.google.com/store/apps/details?id=com.UrDiscFriend" rel="noreferrer"><i className="fa-solid fa-share"></i>Go to Google Playstore</a>
+                              </div>
+                          </div>
+                      </div>
+                      <div className="pro pro__1">
+                        <div className="pro__img">
+                            <a target="_blank" href="https://grauballemikrobryggeri.dk/" rel="noreferrer">
+                                <Image src="/images/GBhjem.png" alt="website" className="transform: translateY(0%); transition: transform 10s ease-in-out 0s;"/></a>
+                            </div>
+                            <div className="pro__text">
+                                <h3>Grauballe Mikrobryggeri</h3>
+                                <p>Beer portfolio website for my dads breewery, where you can explore the past and upcoming beer creations.</p>
+                                <div className="stack">
+                                    <p>HTML</p>
+                                    <p>JavaScript</p>
+                                    <p>CSS</p>
+                                </div>
+                                <div className="links">
+                                    <a target="_blank" href="https://grauballemikrobryggeri.dk/" rel="noreferrer"><i className="fa-solid fa-share"></i>Go to website</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        
+        <section id="contact" className="contact">
+            <div className="container">
+                <h4>Contact</h4>
+                <div className="contact__content">
+                    <div className="contact__info">
+                        <a href="mailto:Rasprivat@hotmail.com">Rasprivat@hotmail.com</a>
+                    </div>
+                </div>
+            </div>
+        </section>
+        
+        <footer>
+            <div className="container">
+                <div className="footerc">
+                    <h3>Copyright © 2023. All rights are reserved</h3>
+                    <div className="footerc__socials">
+                        <a aria-label="linkedin" rel="noreferrer" target="_blank" href="https://www.linkedin.com/in/ingerslev/"><i className="fa-brands fa-linkedin"></i></a>
+                        <a aria-label="github" rel="noreferrer" target="_blank" href="https://github.com/RIngerslev"><i className="fa-brands fa-github"></i></a>
+                    </div>
+                </div>
+            </div>
+        </footer>
+    </div>
   );
 }
