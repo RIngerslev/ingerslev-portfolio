819c09ea9ff5a45d045cea52ef7eb821d4073104
RIngerslev <101323949+RIngerslev@users.noreply.github.com>
2024-11-03 18:17:18 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -20,6 +20,9 @@ export default function Home() {
                       <li>
                           <a href="#contact">Contact</a>
                       </li>
+                      <li>
+                          <a href="#test">test</a>
+                      </li>
                   </ul>
               </div>
         </div>
